e722ec0cc5a8fc1d7b59e6dd9f4e519a9f00b074
Vignesh <88764493+vignesh678@users.noreply.github.com>
2025-04-24 19:14:25 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = https://buyuzdbgnwfdhkusdgur.supabase.co;
+const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1eXV6ZGJnbndmZGhrdXNkZ3VyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1MDA2MjAsImV4cCI6MjA2MTA3NjYyMH0.ZhucwmKQRYRfK0XpzCvNTICMmN2Ag1Y4d5AIgPiC1Vg;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
